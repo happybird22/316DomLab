af83aaee71ab46174a8367fa1deb299eacd72d7d
happybird22 <dungeonsnotdating@gmail.com>
2025-03-24 15:40:44 -0600

    Part 2 completed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,26 @@
-import "./styles/styles.css"
 
+//Part Three
+// Menu data structure
+var menuLinks = [
+    { text: 'about', href: '/about' },
+    { text: 'catalog', href: '/catalog' },
+    { text: 'orders', href: '/orders' },
+    { text: 'account', href: '/account' },
+  ];
+
+//Part One
 const mainEl = document.querySelector(`main`);
 mainEl.style.backgroundColor = `var(--main-bg)`;
 mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
 mainEl.classList.add(`flex-ctr`);
+
+console.log(`mainEl`);
+
+//Part Two
+const topMenuEl = document.getElementById(`top-menu`);
+topMenuEl.style.height = `100%`;
+topMenuEl.style.backgroundColor = `var(--top-menu-bg)`;
+topMenuEl.classList.add(`flex-around`);
+
+//Part Three cont.
 
