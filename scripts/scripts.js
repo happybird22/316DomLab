e3de8af76ab2c021eb2746bf3f4ab93c63951d86
happybird22 <dungeonsnotdating@gmail.com>
2025-03-24 15:20:16 -0600

    Part 1 done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,7 @@
 import "./styles/styles.css"
+
+const mainEl = document.querySelector(`main`);
+mainEl.style.backgroundColor = `var(--main-bg)`;
+mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
+mainEl.classList.add(`flex-ctr`);
+
