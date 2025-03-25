1902d8598f4271daf6cc7972aed761ae7fbc1182
happybird22 <dungeonsnotdating@gmail.com>
2025-03-25 13:55:43 -0600

    updated to callback function for part 3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,7 +9,7 @@ var menuLinks = [
   ];
 
 //Part One
-const mainEl = document.querySelector(`main`);
+let mainEl = document.querySelector(`main`);
 mainEl.style.backgroundColor = `var(--main-bg)`;
 mainEl.innerHTML = `<h1>DOM Manipulation</h1>`;
 mainEl.classList.add(`flex-ctr`);
@@ -17,16 +17,23 @@ mainEl.classList.add(`flex-ctr`);
 console.log(`mainEl`);
 
 //Part Two
-const topMenuEl = document.getElementById(`top-menu`);
+let topMenuEl = document.getElementById(`top-menu`);
 topMenuEl.style.height = `100%`;
 topMenuEl.style.backgroundColor = `var(--top-menu-bg)`;
 topMenuEl.classList.add(`flex-around`);
 
 //Part Three cont.
 
-menuLinks.forEach(function(link) {
-    const aTag = document.createElement(`a`);
-    aTag.setAttribute(`href`, link.href);
-    aTag.textContent = link.text;
-    topMenuEl.appendChild(aTag);
-});
+menuLinks.forEach((link) => {
+  let aTag = document.createElement(`a`);
+  aTag.setAttribute(`href`, link.href);
+  aTag.textContent = link.text;
+  topMenuEl.appendChild(aTag);
+})
+
+// menuLinks.forEach(function(link) {
+//     const aTag = document.createElement(`a`);
+//     aTag.setAttribute(`href`, link.href);
+//     aTag.textContent = link.text;
+//     topMenuEl.appendChild(aTag);
+// });
